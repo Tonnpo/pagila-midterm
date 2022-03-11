fc68bc10503d2fae414057428982187a5e4038af
Tonnpo <Tonn_po@hotmail.com>
2022-03-11 00:50:38 -0800

    Fix problem 2

--- sql/02.sql
+++ sql/02.sql
@@ -5,17 +5,19 @@
  * You cannot hard-code Russell Bacall's actor_id.
  * You're where clause must include a condition using the "first_name" and "last_name" fields of the "actor" table.
  */
-
-SELECT DISTINCT
+SELECT
     COUNT(*)
-FROM actor
-INNER JOIN film_actor USING (actor_id)
-INNER JOIN film USING (film_id)
-INNER JOIN inventory USING (film_id)
-INNER JOIN rental USING (inventory_id)
-INNER JOIN customer USING (customer_id) 
-WHERE
-    actor.first_name = 'RUSSELL'
-AND
-    actor.last_name = 'BACALL'
-;
+FROM (
+    SELECT DISTINCT
+        customer_id 
+    FROM actor
+    INNER JOIN film_actor USING (actor_id)
+    INNER JOIN film USING (film_id)
+    INNER JOIN inventory USING (film_id)
+    INNER JOIN rental USING (inventory_id)
+    INNER JOIN customer USING (customer_id) 
+    WHERE
+        actor.first_name = 'RUSSELL'
+    AND
+        actor.last_name = 'BACALL'
+) t;
